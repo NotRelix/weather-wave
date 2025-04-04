71d70ac070149c8488b32b38eb06346a0f5980e9
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-04-05 02:17:46 +0800

    feat: fetch data from visual crossing api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./style.css";
 
-const test = [];
+const API_KEY = "QVD5PNRPA8ZYADBZWBLL6QQRK";
+const BASE_URL =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+
+async function fetchData(country, BASE_URL, API_KEY) {
+  const response = await fetch(`${BASE_URL}/${country}?key=${API_KEY}`, {
+    mode: "cors",
+  });
+  const data = await response.json();
+  return data;
+}
+
+const country = "new york";
+fetchData(country, BASE_URL, API_KEY).then((response) => {
+  console.log(response);
+});
