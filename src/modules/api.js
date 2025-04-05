31c8dbc501cf7bc53985d5e91279ee0bb3c2ba53
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-04-05 17:08:39 +0800

    feat: process data for the next 5 days

--- src/modules/api.js
+++ src/modules/api.js
@@ -2,17 +2,9 @@ const API_KEY = "QVD5PNRPA8ZYADBZWBLL6QQRK";
 const BASE_URL =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 
-export async function fetchData(country) {
-  const response = await fetch(`${BASE_URL}/${country}?key=${API_KEY}`, {
-    mode: "cors",
-  });
-  const data = await response.json();
-  const necessaryData = processData(data);
-  return necessaryData;
-}
-
-function processData(data) {
-  const currentConditions = {
+function processDataToday(data) {
+  const filteredToday = {
+    resolvedAddress: data.resolvedAddress,
     conditions: data.days[0].conditions,
     datetime: data.days[0].datetime,
     icon: data.days[0].icon,
@@ -24,6 +16,36 @@ function processData(data) {
     humidity: data.days[0].humidity,
     uvindex: data.days[0].uvindex,
   };
-  const address = data.resolvedAddress;
-  return { address, currentConditions };
+  return filteredToday;
+}
+
+function processDataNextFiveDays(data) {
+  const filteredNextFiveDays = [];
+  const nextFiveDays = data.days.slice(1, 6);
+  nextFiveDays.forEach((day) => {
+    const filtered = {
+      conditions: day.conditions,
+      datetime: day.datetime,
+      icon: day.icon,
+      tempmin: day.tempmin,
+      tempmax: day.tempmax,
+    };
+    filteredNextFiveDays.push(filtered);
+  });
+  return filteredNextFiveDays;
+}
+
+function processData(data) {
+  const today = processDataToday(data);
+  const nextFiveDays = processDataNextFiveDays(data);
+  return { today, nextFiveDays };
+}
+
+export async function fetchData(country) {
+  const response = await fetch(`${BASE_URL}/${country}?key=${API_KEY}`, {
+    mode: "cors",
+  });
+  const data = await response.json();
+  const necessaryData = processData(data);
+  return necessaryData;
 }
