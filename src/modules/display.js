b3004c630eeed091f0048b993269d7b425d327e2
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-04-13 14:51:17 +0800

    fix: convert fahrenheit to celsius

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,6 +1,7 @@
+import { convertToCelsius } from "../utils/convertTemp";
 import { convertToMilitaryTime, formatExactDate } from "../utils/formatDate";
 
-export function displayApiContent(response) {
+function displayCardHeader(response) {
   const address = document.querySelector(".address");
   address.textContent = response.today.resolvedAddress;
 
@@ -9,4 +10,22 @@ export function displayApiContent(response) {
 
   const date = document.querySelector(".date");
   date.textContent = formatExactDate(response.today.date);
+}
+
+function displayStatistics(response) {
+  const temperature = document.querySelector(".temperature");
+  const tempnow = response.today.temp;
+  temperature.textContent = `${convertToCelsius(tempnow)}°c`;
+
+  const temperatureAndCondition = document.querySelector(
+    ".temperature-and-condition",
+  );
+  const tempmin = response.today.tempmin;
+  const tempmax = response.today.tempmax;
+  temperatureAndCondition.textContent = `${convertToCelsius(tempmin)}°c / ${convertToCelsius(tempmax)}°c | ${response.nextFiveDays[0].icon}`;
+}
+
+export function displayApiContent(response) {
+  displayCardHeader(response);
+  displayStatistics(response);
 }
